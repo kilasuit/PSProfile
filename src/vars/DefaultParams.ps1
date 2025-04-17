c14180d78a5b0c1b10a0f3e8b20aa066baa84625
Ryan Yates <ryan.Yates@kilasuit.org>
2025-04-17 15:57:07 +0100

    comment out some bits in defaultParams.ps1

--- src/vars/DefaultParams.ps1
+++ src/vars/DefaultParams.ps1
@@ -1,2 +1,15 @@
 $PSDefaultParameterValues['Get-Command:CommandType'] = 'All'
 $PSDefaultParameterValues['*-WinGetPackage:Source'] = 'winget'
+# Flip this to Internal once you have an internal winget &  source
+# $PSDefaultParameterValues['*-WinGetPackage:Source'] = 'internal'
+
+# region to come back to
+<#
+if ($IsWindows) {
+    if
+}
+if ($IsLinux) {}
+#
+$PSDefaultParameterValues['Import-Module:']
+#>
+#endregion to come back to
