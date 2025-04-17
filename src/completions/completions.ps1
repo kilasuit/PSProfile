cf9290b9ed575b64ac953458a840f3214fe65d0c
Ryan Yates <ryan.Yates@kilasuit.org>
2025-04-17 15:55:47 +0100

    completions updates

--- src/completions/completions.ps1
+++ src/completions/completions.ps1
@@ -1,7 +1,14 @@
+# This may not be needed in the future depending on future PSTeam work in this area
 Import-Module CompletionPredictor
-Import-Module Microsoft.WinGet.CommandNotFound
-
-# Think I have this elsewhere
+## This Module will throw a wobbler if there is no internet connectivity, so much so
+## it will forcefully terminate the process that imports it
+# as such I have added a check to see if the system is connected to the internet
+# and if not, it will not import the module
+# This is a bit of a hack but it works for now & I need to report this to Winget team as per Issue#19
+if ((Get-NetConnectionProfile).IPv4Connectivity -NotMatch 'LocalNetwork') {
+    Import-Module Microsoft.WinGet.CommandNotFound
+}
+# Think I have this elsewhere though may bring it in here again see Issue#20
 #foreach ($file in Get-ChildItem -Path $($Profile.MyProfileDirectory)\src\completions\*\*.ps1 ) {
 #     . $file.FullName
 # }
