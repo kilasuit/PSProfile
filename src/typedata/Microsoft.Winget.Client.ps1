bf6f7d46a89d987786d6aaaea4e58953a4e8f1c8
Ryan Yates <ryan.yates@kilasuit.org>
2025-05-09 06:30:31 +0100

    minor change to var name for future additions

--- src/typedata/Microsoft.Winget.Client.ps1
+++ src/typedata/Microsoft.Winget.Client.ps1
@@ -1,7 +1,7 @@
-$typeDataParams = @{
+$WingetTypeDataParams = @{
     TypeName   = 'Microsoft.WinGet.Client.Engine.PSObjects.PSInstalledCatalogPackage'
     MemberType = 'AliasProperty'
     MemberName = 'IUA'
     Value      = 'IsUpdateAvailable'
     }
-Update-TypeData @typeDataParams
+Update-TypeData @WingetTypeDataParams
