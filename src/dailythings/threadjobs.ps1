c1b5b4eeedd1a44cdd89090ff126ae9543ebc00e
Ryan Yates <ryan.yates@kilasuit.org>
2025-05-09 06:31:02 +0100

    additional threadjobs

--- src/dailythings/threadjobs.ps1
+++ src/dailythings/threadjobs.ps1
@@ -1,1 +1,4 @@
 Start-ThreadJob -Name UpdateHelp {Update-Help -force}
+# Start-ThreadJob -Name ModulesToUpdate {Update-PSResource -force}
+# Start-ThreadJob -Name AppsToUpdate {Get-WinGetPackage | Where IsUpdateAvailable}
+# Start-ThreadJob -Name RemoteMachineAppsToUpdate {Invoke-Command -Session $Sessions { Get-WinGetPackage | Where IsUpdateAvailable} }
