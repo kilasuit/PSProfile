a1b597a391b04081ffefde275f6baef16acaa1c4
Ryan Yates <ryan.yates@kilasuit.org>
2025-05-10 11:10:06 +0100

    additional default params

--- src/vars/DefaultParams.ps1
+++ src/vars/DefaultParams.ps1
@@ -1,15 +1,14 @@
+# region winget
+
 $PSDefaultParameterValues['Get-Command:CommandType'] = 'All'
 $PSDefaultParameterValues['*-WinGetPackage:Source'] = 'winget'
-# Flip this to Internal once you have an internal winget &  source
+# Flip this to Internal once you have an internal winget source setup
 # $PSDefaultParameterValues['*-WinGetPackage:Source'] = 'internal'
 
-# region to come back to
-<#
-if ($IsWindows) {
-    if
-}
-if ($IsLinux) {}
-#
-$PSDefaultParameterValues['Import-Module:']
-#>
-#endregion to come back to
+#endregion winget
+
+
+## Remoting DefaultVars
+$PSDefaultParameterValues['*-PSSession:ConfigurationName'] ='PowerShell.7-preview'
+$PSDefaultParameterValues['Invoke-Command:ConfigurationName'] ='PowerShell.7-preview'
+
