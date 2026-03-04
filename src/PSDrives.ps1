7cd090acaa377a57069d5ba77512a0bc4c916b21
Ryan Yates <ryan.Yates@kilasuit.org>
2026-03-04 13:07:27 +0000

    fixed psdrives for now

--- src/PSDrives.ps1
+++ src/PSDrives.ps1
@@ -21,11 +21,35 @@ New-PSDrive -Name Public -PSProvider FileSystem -Root 'Code:\Mine\pub\' | Out-Nu
 New-PSDrive -Name PriGH -PSProvider FileSystem -Root 'Code:\Mine\pri\Github\' | Out-Null
 New-PSDrive -Name PubGH -PSProvider FileSystem -Root 'Code:\Mine\pub\Github\' | Out-Null
 
+New-PSDrive -Name PubDocRepos -PSProvider FileSystem -Root 'PriGH:\kilasuit\Docs' | Out-Null
+
 New-PSDrive -Name Scripts -PSProvider FileSystem -Root 'PriGH:\kilasuit\Scripts' | Out-Null
 New-PSDrive -Name ScriptsWIP -PSProvider FileSystem -Root 'PriGH:\kilasuit\Scripts-WIP' | Out-Null
 New-PSDrive -Name ModulesWIP -PSProvider FileSystem -Root 'PriGH:\kilasuit\Modules-WIP' | Out-Null
+
+# GitHub Organizations
+New-PSDrive -Name getpsuguk -PSProvider FileSystem -Root 'PubGH:\get-psuguk' | Out-Null
+New-PSDrive -Name PSOrgUK -PSProvider FileSystem -Root 'PubGH:\PowerShellOrgUK' | Out-Null
+New-PSDrive -Name DSC-Org -PSProvider FileSystem -Root 'PubGH:\DSCCommunity' | Out-Null
+New-PSDrive -Name PS-GH -PSProvider FileSystem -Root 'PubGH:\PowerShell' | Out-Null
+#New-PSDrive -Name PS-Modules -PSProvider FileSystem -Root 'PubGH:\PowerShellModules' | Out-Null
+
+
+#New-PSDrive -Name PSOrgDev -PSProvider FileSystem -Root 'PubGH:\PowerShellOrgDev' | Out-Null
+
+#New-PSDrive -Name Modules -PSProvider FileSystem -Root 'PriGH:\kilasuit\Modules' | Out-Null
+
+
+# Sites
 New-PSDrive -Name Blog -PSProvider FileSystem -Root 'PriGH:\kilasuit\blogsite' | Out-Null
+New-PSDrive -Name kk -PSProvider FileSystem -Root 'PriGH:\kilasuit\sites\kilas-kitchen' | Out-Null # kk is short for kila's kitchen
 New-PSDrive -Name Mhasl -PSProvider FileSystem -Root 'PriGH:\mhaslme\website' | Out-Null
+
+# Not yet created
+# New-PSDrive -Name PSugUK -PSProvider FileSystem -Root 'PubGH:\PowerShellOrgUK\site' | Out-Null
+# New-PSDrive -Name INFamily -PSProvider FileSystem -Root 'PubGH:\InteractionNotes\site' | Out-Null
+#New-PSDrive -Name PubMhasl -PSProvider FileSystem -Root 'PubGH:\mhaslme\website' | Out-Null
+
 New-PSDrive -Name Profile -PSProvider FileSystem -Root 'PriGH:\kilasuit\PSProfile' | Out-Null
 
 
