c12d10702f8e7fdc77d2a9a195172fe953347545
Ryan Yates <ryan.Yates@kilasuit.org>
2026-08-28 11:33:48 +0100

    update-help thread job only on Mondays

--- src/dailythings/threadjobs.ps1
+++ src/dailythings/threadjobs.ps1
@@ -1,4 +1,6 @@
-Start-ThreadJob -Name UpdateHelp {Update-Help -force}
+if ([DateTime]::Now.DayOfWeek -eq 'Monday' -and [System.Environment]::GetEnvironmentVariable('UpdateHelp','User') -ne (get-date -Format "yyyyMMdd")) {
+    Start-ThreadJob -Name UpdateHelp {Update-Help -force; [System.Environment]::SetEnvironmentVariable("UpdateHelp", (get-date -Format "yyyyMMdd"), [System.EnvironmentVariableTarget]::User)}
+}
 # Start-ThreadJob -Name ModulesToUpdate {Update-PSResource -force}
 # Start-ThreadJob -Name AppsToUpdate {Get-WinGetPackage | Where IsUpdateAvailable}
 # Start-ThreadJob -Name RemoteMachineAppsToUpdate {Invoke-Command -Session $Sessions { Get-WinGetPackage | Where IsUpdateAvailable} }
